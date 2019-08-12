ace2d0e05a0bf94426f5002d2a3c2f9cb13c2abb
road2hana <road2hana_2018@outlook.com>
2019-08-12 22:29:46 +0800

    Training the Network

--- p4/naive_em.py
+++ p4/naive_em.py
@@ -37,7 +37,28 @@ def mstep(X: np.ndarray, post: np.ndarray) -> GaussianMixture:
     Returns:
         GaussianMixture: the new gaussian mixture
     """
-    raise NotImplementedError
+    n, K , d = post.shape[0], post.shape[1], X.shape[1]
+    # hat_n_k = sum( p(k | i) )
+    hat_n_k = np.sum(post, axis=0) # 1 x K
+    hat_n_k = hat_n_k.reshape(K, 1) # K x 1
+
+    # hat_mix_p = hat_n_k/n
+    hat_mix_p = hat_n_k/n  # K x 1
+    hat_mix_p = hat_mix_p.reshape(K)
+
+    # hat_mu_k = (1/hat_n_k) * sum( p(k|i)*x_i )
+    hat_mu_k = post.T.dot(X)/hat_n_k # K x d
+
+    # hat_var = (1/(hat_n_k * d)) * sum(p(k|i) *  (x_i - hat_mu_k)**2 )
+    xx_mu_2 = np.square((X.reshape(n, 1, d) - hat_mu_k.reshape(1, K, d)), dtype=np.float) # n x K x d
+    xx_mu_2 = np.sum(xx_mu_2, axis=2, dtype=np.float) # n x K
+    prob_xx_mu_2 = post * xx_mu_2 # n x K
+    # hat_var_k = (1/(hat_n_k*d)) * sum(post * (xx - mu)**2 )
+    hat_var_k = np.sum(prob_xx_mu_2, axis=0, dtype=np.float)/(d * hat_n_k.reshape(1, K)) # 1 x K
+    hat_var_k = hat_var_k.reshape(K)
+
+    mixture = GaussianMixture(hat_mu_k, hat_var_k, hat_mix_p)
+    return mixture
 
 
 def run(X: np.ndarray, mixture: GaussianMixture,
