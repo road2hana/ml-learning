d94871cb7a8706e3b7df875defbddf08b3ea6ebb
road2hana <road2hana_2018@outlook.com>
2019-08-12 22:30:39 +0800

    Training the Network

--- p4/naive_em.py
+++ p4/naive_em.py
@@ -65,6 +65,7 @@ def run(X: np.ndarray, mixture: GaussianMixture,
         post: np.ndarray) -> Tuple[GaussianMixture, np.ndarray, float]:
     """Runs the mixture model
 
+
     Args:
         X: (n, d) array holding the data
         post: (n, K) array holding the soft counts
