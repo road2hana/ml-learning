fd57330ef6bf55562e34ffe20f28f491b4f9f792
road2hana <road2hana_2018@outlook.com>
2019-08-15 22:14:42 +0800

    Training the Network

--- p4/em.py
+++ p4/em.py
@@ -18,7 +18,28 @@ def estep(X: np.ndarray, mixture: GaussianMixture) -> Tuple[np.ndarray, float]:
         float: log-likelihood of the assignment
 
     """
-    raise NotImplementedError
+    n, d, K = X.shape[0], X.shape[1], mixture.p.shape[0]
+    # calculate Cu
+    c_u = 1 - (X==0).astype(int) # n x d
+    c_u_d = (np.sum(c_u, axis=1)).reshape(n,1) # n x 1
+    X_mu = X.reshape(n, 1, d) - mixture.mu.reshape(1, K, d) # n x K x d
+    X_mu = X_mu*c_u.reshape(n, 1, d)
+    X_mu_square = np.sum(np.square(X_mu, dtype=np.float), axis=2)  # n x K
+    X_mu_square_var = - X_mu_square/(2*mixture.var.reshape(1, K)) # n x K
+
+    # For this function, you will want to use log(mixture.p[j] + 1e-16) instead of log(mixture.p[j]) to avoid numerical underflow
+    # f(u,j) = log(pi) + log(N(X|theta))
+    log_N = X_mu_square_var - (c_u_d/2)*np.log(2*np.pi*mixture.var.reshape(1, K)) # n x K
+    f_u_i = np.log((mixture.p.reshape(1, K)+1e-16),) + log_N # n x K
+    ll = logsumexp(f_u_i, axis=1) # n x 1
+    ll = np.sum(ll) # scalar
+    # l(j|u) = f(u,j) - log_sum(exp(f(u,j))
+    # log_sum(exp(f(u,j)) = x_star + log_sum(x - x_star), x_star = max(x)
+    f_u_i_star = np.max(f_u_i, axis=1) # n
+    l_j_u = f_u_i - (f_u_i_star.reshape(n,1) + logsumexp(f_u_i - f_u_i_star.reshape(n, 1), axis=1).reshape(n,1)) # n x K
+
+    p_j_u = np.exp(l_j_u)
+    return p_j_u, ll
 
 
 
@@ -37,7 +58,34 @@ def mstep(X: np.ndarray, post: np.ndarray, mixture: GaussianMixture,
     Returns:
         GaussianMixture: the new gaussian mixture
     """
-    raise NotImplementedError
+    n, d, k = X.shape[0], X.shape[1], mixture.var.shape[0]
+    # hat_p = sum( p(j|u) )
+    hat_p = np.sum(post, axis=0)/n # k
+
+    # delta_l_c_u is indicator matrix of X which cell is not equal zero
+    delta_l_c_u = 1 - (X==0).astype(int) # n x d
+
+    # delta(l, c_u)*p(j|u) is a mechanism to leave mu of missing value cell unchanged
+    delta_p = delta_l_c_u.reshape(n,1,d) * post.reshape(n,k,1) # n x k x d
+    delta_p_sum_n = np.sum(delta_p, axis=0) # k x d
+
+    row_mu = np.sum(delta_p * X.reshape(n,1,d), axis=0) # k x d
+    hat_mu = (row_mu/np.sum(delta_p, axis=0))  # k x d
+
+    much_less_mu_index = delta_p_sum_n<1
+    hat_mu[much_less_mu_index] = mixture.mu[much_less_mu_index]
+
+
+    # hat_sigma_square = (1/(sum( dim(u) * p(j|u)))) * sum( p(j|u) * norm(X - hat_mu)**2 )
+    dim_u = np.sum(delta_l_c_u, axis=1) # n x 1
+    dim_u_post = dim_u.reshape(n,1) * post # n x k
+    x_error = (X.reshape(n, 1, d) - hat_mu.reshape(1, k, d))*delta_l_c_u.reshape(n,1,d) # n x k x d
+    x_error_square = np.sum(np.square(x_error), axis=2) # n x k
+    hat_sigma_square = (1/np.sum(dim_u_post, axis=0)) * np.sum(post * x_error_square, axis=0) # k
+    hat_sigma_square[hat_sigma_square < min_variance] = min_variance
+    mixture = GaussianMixture(hat_mu, hat_sigma_square, hat_p)
+
+    return mixture
 
 
 def run(X: np.ndarray, mixture: GaussianMixture,
--- p4/test.py
+++ p4/test.py
@@ -10,3 +10,17 @@ n, d = X.shape
 seed = 0
 
 # TODO: Your code here
+
+mu = np.array([[2., 4., 5., 5., 0.],[3., 5., 0., 4., 3.], [2., 5., 4., 4., 2.], [0., 5., 3., 3., 3.]])
+
+var = np.array([5.93, 4.87, 3.99, 4.51])
+pi = np.array([0.25, 0.25, 0.25, 0.25])
+
+mixture = common.GaussianMixture(mu, var, pi)
+post, ll = em.estep(X, mixture)
+
+print(post)
+print(ll)
+
+mixture = em.mstep(X, post, mixture)
+print(mixture)
